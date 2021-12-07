63215201799cf661f5b22efaf4635217caa390dd
kunal gaur <42763666+kunalgaur7983@users.noreply.github.com>
2021-12-07 09:36:31 +0530

    Gesture Control Sound

---
+++ Gesture Sound/main.py
@@ -0,0 +1,51 @@
+import cv2
+import time
+import numpy as np
+import math
+
+import handtracking as htm
+
+###########################
+wcam, hcam = 640, 640
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wcam)
+cap.set(4, hcam)
+ptime = 0
+
+detector=htm.handDetector(detectionCon=0.7)
+
+while True:
+    success, img = cap.read()
+
+    img= detector.findHands(img)
+    lmList=detector.findPosition(img,draw=False)
+    if len(lmList)!=0:
+        print(lmList[4],lmList[8])
+
+        x1,y1=lmList[4][1],lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx,cy=(x1+x2)//2,(y1+y2)//2
+
+        cv2.circle(img,(x1,y1),15,(255,0,255),cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
+
+        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+        length=math.hypot(x2-x1,y2-y1)
+
+        if(length<=50):
+            cv2.circle(img,(cx,cy),15,(0,255,0),cv2.FILLED)
+
+
+
+    ctime = time.time()
+    fps = 1 / (ctime - ptime)
+    ptime = ctime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
